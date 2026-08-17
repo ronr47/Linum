0d2ba71a8d120b814f112a31f9b4fc342991bea4
Ron <ron@localhost>
2026-08-17 10:44:52 -0700

    add semantic diagnostic exception type

--- src/diagnostics/__init__.py
+++ src/diagnostics/__init__.py
@@ -1,9 +1,11 @@
 from .span import SourceSpan, UNKNOWN_SPAN
 from .errors import LinumDiagnostic, DiagnosticError
+from .semantic import SemanticError
 
 __all__ = [
     "SourceSpan",
     "UNKNOWN_SPAN",
     "LinumDiagnostic",
     "DiagnosticError",
+    "SemanticError",
 ]
